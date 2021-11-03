a8258b88a53f06722faca25187b210a19c841175
star.knt.8@gmail.com <star.knt.8@gmail.com>
2021-11-03 12:43:35 +0700

    update feature auction - cant view detail before start auction product

--- src/components/ShefingCard.js
+++ src/components/ShefingCard.js
@@ -1,4 +1,4 @@
-import { Col} from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card'
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { Link, Redirect } from 'react-router-dom';
@@ -39,7 +39,7 @@ export default function AuctionHistoryCard({ item }) {
     const defaultImg = '../../public/images.png/100px250';
     //const [like, setlike] = useState();
     const dispatch = useDispatch();
-   
+
     const history = useHistory();
 
 
@@ -84,10 +84,23 @@ export default function AuctionHistoryCard({ item }) {
                             <br />
                             Lượt đấu giá: {data.count_auction}
                             <br />
-                            <Link to={`/product/detail?productid=${data.product_id}`} style={{ fontSize: '0.6rem' }}>Xem chi tiết</Link>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <Link to={`/user/update-post-product?productid=${data.product_id}`} style={{ fontSize: '0.6rem' }}>Cập nhật</Link>
+                            <Row>
+                                {
+                                    new Date().getTime() - new Date(item.start_day).getTime() > 0 ?
+                                        <Col className='pl-2'>
+                                            <Link to={`/product/detail?productid=${data.product_id}`} style={{ fontSize: '0.6rem' }}>
+                                                Xem chi tiết
+                                            </Link>
+                                        </Col>
+                                        : <Col className='pl-2'>
+                                        </Col>
+                                }
 
+                                <Col className='p-0'>   <Link to={`/user/update-post-product?productid=${data.product_id}`} style={{ fontSize: '0.6rem' }}>Cập nhật</Link> </Col>
+                            </Row>
+                            {/* <Link to={`/product/detail?productid=${data.product_id}`} style={{ fontSize: '0.6rem' }}>Xem chi tiết</Link>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Link to={`/user/update-post-product?productid=${data.product_id}`} style={{ fontSize: '0.6rem' }}>Cập nhật</Link> */}
 
                         </Card.Text>
                         {/* <Row >
